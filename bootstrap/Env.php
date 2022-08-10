2e7f30bfecafbc3d6287e7cef021978fdadaae32
pgon66 <110068885+pgon66@users.noreply.github.com>
2022-08-10 18:51:12 -0300

    Aula PHP - 09/08/2022

--- bootstrap/Env.php
+++ bootstrap/Env.php
@@ -1,7 +1,23 @@
 <?php
 
 class Env {
-    public function __constructc() {
-        
+
+    public static function execute() {
+        $contentOfEnvFile = file_get_contents(__DIR__ . "\..\.env");
+        $arrayEnv = explode("\n",$contentOfEnvFile);
+
+        foreach ($arrayEnv as $value) {
+            $keyAndValue = explode("=", $value);
+
+            if (!isset($keyAndValue[1])){
+                continue;
+            }
+
+            $nameOfVariable = $keyAndValue[0];
+            $valueOfVariable =  $keyAndValue[1];
+            
+            $_ENV[$nameOfVariable] = $valueOfVariable;
+        }
+ 
     }
 }
