600e3dd20f4b67cb6e55adeb6cdbc996a9fe6d8b
pgon66 <110068885+pgon66@users.noreply.github.com>
2022-09-27 22:23:57 -0300

    Aula 27/09/2022

--- app/Controllers/InsertDataController.php
+++ app/Controllers/InsertDataController.php
@@ -12,20 +12,45 @@ class InsertDataController extends AbstractControllers{
     public function exec() {
         $pdo = DatabaseConnection::start()->getPDO();
         $params = $this->processServerElements->getInputJSONData();
+        $response = ['sucess' => true];
+        $attrName;
 
-        $query = "INSERT INTO user (name, last_name, age) VALUES (:name,:last_name,:age)";
-        
-        $statement = $pdo->prepare($query);
+        try {
 
-        $statement->execute([
-            ':name' => $params["name"],
-            ':last_name' => $params["lastName"],
-            ':age' => $params["age"]
-        ]);
+            if (!$params['name']) {
+                $attrName = 'name';
+                throw new \Exception('the name has to be send in the request');
+            }
+    
+            if (!$params['last_name']) {
+                $attrName = 'lat_name';
+                throw new \Exception('the last_name has to be send in the request');
+            }
+    
+            if (!$params['age']) {
+                $attrName = 'age';
+                throw new \Exception('the age has to be send in the request');
+            }
+    
+            $query = "INSERT INTO user (name, last_name, age) VALUES (:name,:last_name,:age)";
+            
+            $statement = $pdo->prepare($query);
+    
+            $statement->execute([
+                ':name' => $params["name"],
+                ':last_name' => $params["lastName"],
+                ':age' => $params["age"]
+            ]);
 
-        view([
-            'success'=> true
-        ]);
+        } catch (\Exception $e) {
+            $response = [
+                'success' => false, 
+                'message' => $e->getMessage(),
+                'missingAttribute' => $attrName
+            ];
+        }
+
+        view([$response]);
     }
 
 }
